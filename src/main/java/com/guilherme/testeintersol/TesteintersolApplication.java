dcffc4dd41bb10d105c9c3ee47150da8f3f04210
Guilherme Bezerra <guilhermebfneto@gmail.com>
2023-03-08 09:59:36 -0300

    Commit inicial

---
+++ src/main/java/com/guilherme/testeintersol/TesteintersolApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.testeintersol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteintersolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteintersolApplication.class, args);
+	}
+
+}
